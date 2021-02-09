9f9bd5acc5d0e6a7e64544e5eefa60ea0bac17b5
byu163 <72476996+byu163@users.noreply.github.com>
2021-02-09 16:08:45 -0500

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,17 +1,36 @@
 
-#include <stddef.h>
+#include <stdint.h>
 #include "BasicRenderer.h"
 #include "cstr.h"
+#include "efiMemory.h"
+#include "memory.h"
 
-extern "C" void _start(Framebuffer* framebuffer, PSF1_FONT* psf1_font){
+struct BootInfo{
+	Framebuffer* framebuffer;
+	PSF1_FONT* psf1_Font;
+	EFI_MEMORY_DESCRIPTOR* mMap;
+	uint64_t mMapSize;
+	uint64_t mMapDescSize;
+};
+
+extern "C" void _start(BootInfo* bootInfo){
     
-	BasicRenderer newRenderer = BasicRenderer(framebuffer, psf1_font);
-	newRenderer.Print(to_string((uint64_t) 1234976));
-	newRenderer.CursorPosition = {0, 16};
-	newRenderer.Print(to_string((int64_t) -1234976));
-	newRenderer.CursorPosition = {0, 32};
-	newRenderer.Print(to_string((double) -13.14));
-	newRenderer.CursorPosition = {0, 48};
-	newRenderer.Print(to_string((uint64_t) 0x1234));
-    return ;
+	BasicRenderer newRenderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_Font);
+
+	uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
+
+	newRenderer.Print(to_string(GetMemorySize(bootInfo->mMap, mMapEntries, bootInfo->mMapDescSize)));
+	
+	// for (int i = 0; i < mMapEntries; i++){
+	// 	EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescSize));
+	// 	newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+	// 	newRenderer.Print(EFI_MEMORY_TYPE_STRINGS[desc->type]);
+	// 	newRenderer.Color = 0xffff00ff;
+	// 	newRenderer.Print(" ");
+	// 	newRenderer.Print(to_string(desc->numPages *  4096 / 1024));
+	// 	newRenderer.Print(" KB");
+	// 	newRenderer.Color = 0xffffffff;
+	// }
+
+	return ;
 }
